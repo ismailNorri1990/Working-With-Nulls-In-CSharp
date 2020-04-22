8cba0ddef975ad5fe0fa332571f7fefee5855945
Ismail NORRI <ismail.norri.24@gmail.com>
2020-04-22 17:26:16 +0000

    Using Nullable -  ? =>Nullable , delete magic values from constructor.

--- GameConsole/PlayerCharacter.cs
+++ GameConsole/PlayerCharacter.cs
@@ -10,13 +10,11 @@ namespace GameConsole
     {
         public PlayerCharacter()
         {
-            DateOfBirth = null ; //Using Nullabke
-            DaySinceLastLogin = null ; //Using Nullable
         }
 
         public string Name { get; set; }
         public int? DaySinceLastLogin { get; set; }
-        public Nullable<DateTime> DateOfBirth { get; set; }
+        public DateTime? DateOfBirth { get; set; }
 
         public bool? IsNoob { get; set; }
     }
