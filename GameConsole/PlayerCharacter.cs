1f3c2a8d76704248a0464246409586470bbdaa39
Ismail NORRI <ismail.norri.24@gmail.com>
2020-04-22 16:49:32 +0000

    Ajoutez des fichiers projet.

---
+++ GameConsole/PlayerCharacter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameConsole
+{
+    class PlayerCharacter
+    {
+        public PlayerCharacter()
+        {
+            DateOfBirth = DateTime.MinValue; //Magic Number
+            DaySinceLastLogin = -1; //Magic Number
+        }
+
+        public string Name { get; set; }
+        public int DaySinceLastLogin { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
---
+++ GameConsole/PlayerDisplayer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameConsole
+{
+    class PlayerDisplayer
+    {
+        public static void Write(PlayerCharacter playerCharacter)
+        {
+            Console.WriteLine(playerCharacter.Name);
+
+            if(playerCharacter.DaySinceLastLogin == -1)
+            {
+                Console.WriteLine("No value for Day SinceLastLogin");
+            }
+            else
+            {
+                Console.WriteLine(playerCharacter.DaySinceLastLogin);
+            }
+
+
+           if (playerCharacter.DateOfBirth == DateTime.MinValue)
+            {
+                Console.WriteLine("No value for Birthday");
+            }
+            else
+            {
+                Console.WriteLine(playerCharacter.DateOfBirth);
+            }
+
+        }
+            
+    }
+}
